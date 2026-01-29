e0ad52c3d92563bf6fc5f29c92f8319d449cc8ef
PrasadGurav13 <prasadgurav8551@gmail.com>
2026-01-30 01:17:15 +0530

    user model change and signup

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,6 +3,39 @@ import bcrypt from 'bcrypt'
 import {NextFunction, Request, response, Response} from 'express'
 import { generateToken } from '~/utils/jwt.util';
 
+export const signupController = async ( req: Request, res: Response, next: NextFunction ) => {
+    const { email, password, firstName, lastName, mobileNo } = req.body;
+    const usermail = email.trim().toLowerCase();
+
+    const userAlreadyExist = await prisma.user.findFirst({
+        where:{
+            email: usermail,
+            isDeleted: false
+        }
+    })
+
+    if (userAlreadyExist) {
+        return next("user already exist")
+    }
+
+    const userData = await prisma.user.create({
+        data: {
+           firstName: firstName,
+           lastName: lastName,
+           mobileNo: mobileNo,
+           email: usermail,
+           password: bcrypt.hashSync(password, 10),
+        }
+    })
+
+    const token = generateToken(userData, '30d');
+
+    return res.json({
+        token,
+        userData
+    })
+} 
+
 export const loginController = async (req: Request, res: Response, next: NextFunction) =>{
 
     const { email, password } = req.body;
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { loginController } from "~/controllers/auth.controller";
+import { loginController, signupController } from "~/controllers/auth.controller";
 
 
 const authRouter = Router();
 
 authRouter.post("/login", loginController);
+
+authRouter.post("/signup", signupController);
 
 export default authRouter;
